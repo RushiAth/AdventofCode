6a61a533cfe475ee74879d6c25c8227b2dec62fe
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-06 10:33:11 -0500

    day6

---
+++ Advent2021/problem6.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+def day(fishD):
+    newFishD = {}
+
+    for time in range(8, -1, -1):
+        if time != 0:
+            newFishD[time-1] = fishD[time]
+        else:
+            newFishD[8] = fishD[0]
+            newFishD[6] += fishD[0]
+
+    return newFishD
+
+handIn = open('input6.txt')
+
+content = Counter([int(num) for num in (handIn.read()).split(',')])
+
+for i in range(256):
+    content = day(content)
+
+sum = 0
+
+for num in content.values():
+    sum += num
+
+print(sum)
