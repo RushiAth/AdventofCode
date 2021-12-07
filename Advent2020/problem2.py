ff06175d98ded37118ec30c58860329278a1364f
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-07 15:03:50 -0500

    2020day2

--- Advent2020/problem2.py
+++ Advent2020/problem2.py
@@ -3,15 +3,19 @@ import re
 handIn = open('input2020_2.txt')
 
 content = [line.strip() for line in handIn.readlines()]
-
-policyPass = {}
+print(len(content))
+counter = 0
 
 for index in range(len(content)):
     line = content[index].split(': ')
     password = line[1]
-    policyNum = line[0].split()[0]
+    policyNums = line[0].split()[0].split('-')
     policyLetter = line[0].split()[1]
 
-    
-    
+    letterInPass = password.count(policyLetter)
+
+    if int(policyNums[0]) <= letterInPass <= int(policyNums[1]):
+        counter += 1
+
+print(counter)
     
