ee2b4208c4ea6649a8cca286dbfe1d84bf6c6282
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-08 10:28:47 -0500

    day8

--- Advent2021/problem8.py
+++ Advent2021/problem8.py
@@ -1,13 +1,28 @@
+def findNumSum(input, output):
+    numSum = 0
+
+
+    return numSum
+
+
 handIn = open('input8.txt')
 
 content = [(line.strip()).split(' | ') for line in (handIn.readlines())]
-counter = 0
+totalSum = 0
 
+for index in range(len(content)):
+    totalSum += findNumSum(content[index][0], content[index][1])
+
+
+#part 1
+'''
+counter = 0
 for index in range(len(content)):
     value = content[index][1].split()
 
     for i in value:
         if len(i) in [2, 3, 4, 7]:
-            counter += 1
+            sum += 1
 
 print(counter)
+'''
