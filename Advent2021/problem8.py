5fa0ebbc7c7a4908dbd0ada8669118ec569a73e9
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-08 12:13:57 -0500

    day8

--- Advent2021/problem8.py
+++ Advent2021/problem8.py
@@ -1,29 +1,9 @@
-def mappingNum():
-    mapping = []
-
-
-    return mapping
-
-def findNumSum(input, output):
-    numSum = 0
-
-
-    return numSum
-
-
-handIn = open('input8.txt')
-
+handIn = open('testingInput.txt')
 content = [(line.strip()).split(' | ') for line in (handIn.readlines())]
-totalSum = 0
-
-for index in range(len(content)):
-    totalSum += findNumSum(content[index][0], content[index][1])
-
-
 
 #part 1
-'''
 counter = 0
+
 for index in range(len(content)):
     value = content[index][1].split()
 
@@ -32,4 +12,40 @@ for index in range(len(content)):
             sum += 1
 
 print(counter)
-'''
+
+
+#part 2
+def mappingNum(input):
+    mapping = {'top': '', 'middle': '', 'bottom': '', 'topright': '', 'topleft': '', 'bottomright': '', 'bottomleft': ''}
+
+    input = sorted(input.split())
+    print(input)
+
+    for letters in input:
+        if len(letters) == 2:
+            mapping['topright'] = letters[0]
+            mapping['bottomright'] = letters[1]
+    
+    for letters in input:
+        for letter in letters:
+            if letter in mapping.values():
+                continue
+                
+
+    return mapping
+
+def findNumSum(input, output):
+    numSum = 0
+
+    letterPositions = mappingNum(input)
+
+    print(letterPositions)
+
+    return numSum
+
+totalSum = 0
+
+for index in range(len(content)):
+    totalSum += findNumSum(content[index][0], content[index][1])
+
+print(totalSum)
