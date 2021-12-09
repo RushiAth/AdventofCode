55a38d6fd96eed12856646b5b288104a64d82262
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-09 16:12:30 -0500

    day9

--- Advent2021/problem9.py
+++ Advent2021/problem9.py
@@ -3,15 +3,15 @@ content = [line.strip() for line in handIn.readlines()]
 
 def checkSurrounding(num, index1, index2, lst):
     surrounding = [lst[index1-1][index2], lst[index1][index2-1], lst[index1][index2+1], lst[index1+1][index2]]
-
     tf = [num < SNum for SNum in surrounding]
 
     return all(tf) 
 
-def basin(num, index1, index2, lst):
+def basin(basins, lst):
+    size = 0
 
 
-    return None
+    return size
 
 for index in range(len(content)):
     content[index] = '9' + content[index] + '9'
@@ -21,14 +21,31 @@ content.append('9'*len(content[0]))
 
 #Part 1
 risk = 0
-basinCenter = {}
+basinCenters = {}
 
 for index in range(1, len(content)-1):
     for index2 in range(1, len(content[index])-1):
         if checkSurrounding(content[index][index2], index, index2, content) == True:
-            basinCenter[(index, index2)] = content[index][index2]
+            basinCenters[(index, index2)] = content[index][index2]
             risk += (int(content[index][index2]) + 1)
 
 print(risk)
 
 #Part 2
+basinSizes = []
+print(basinCenters)
+
+for b in basinCenters:
+    basinSizes.append(basin(basinCenters, content))
+
+print(sorted(basinSizes))
+
+
+# for i in content:
+#     for j in i:
+#         if j == '9':
+#             print('\033[1;31;40mX', end='')
+#         else:
+#             print('\033[1;32;40m.', end='')
+#     print()
+# print('\033[0;37;40m')
