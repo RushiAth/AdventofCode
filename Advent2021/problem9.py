5a1e44f3445b8eb198fcf37a3e31c591590ed12f
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-09 15:18:43 -0500

    day9

--- Advent2021/problem9.py
+++ Advent2021/problem9.py
@@ -1,12 +1,17 @@
-handIn = open('input9.txt')
+handIn = open('testingInput.txt')
 content = [line.strip() for line in handIn.readlines()]
 
-def checkS(num, index1, index2, lst):
+def checkSurrounding(num, index1, index2, lst):
     surrounding = [lst[index1-1][index2], lst[index1][index2-1], lst[index1][index2+1], lst[index1+1][index2]]
 
     tf = [num < SNum for SNum in surrounding]
 
     return all(tf) 
+
+def basin(num, index1, index2, lst):
+
+
+    return None
 
 for index in range(len(content)):
     content[index] = '9' + content[index] + '9'
@@ -14,11 +19,16 @@ for index in range(len(content)):
 content.insert(0, '9'*len(content[0]))
 content.append('9'*len(content[0]))
 
+#Part 1
 risk = 0
+basinCenter = {}
 
 for index in range(1, len(content)-1):
     for index2 in range(1, len(content[index])-1):
-        if checkS(content[index][index2], index, index2, content) == True:
+        if checkSurrounding(content[index][index2], index, index2, content) == True:
+            basinCenter[(index, index2)] = content[index][index2]
             risk += (int(content[index][index2]) + 1)
 
 print(risk)
+
+#Part 2
