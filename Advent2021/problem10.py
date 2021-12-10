6c1a924671c0d8e5073fe21def9925a7fdb1a924
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-10 16:42:21 -0500

    day10

--- Advent2021/problem10.py
+++ Advent2021/problem10.py
@@ -10,11 +10,25 @@ def checkError(character):
         return 1197
     else:
         return 25137
+
+def checkScore(character):
+    if character == ')':
+        return 1
+    elif character == ']':
+        return 2
+    elif character == '}':
+        return 3
+    else:
+        return 4
         
-ending = []
 sumError = 0
+counter = 0
+indexOfCorrupt = []
+listOfEndings = []
 
 for line in content:
+    ending = []
+
     for char in line:
         if char == '(':
             ending.append(')')
@@ -34,6 +48,22 @@ for line in content:
             ending.pop()
         else:
             sumError += checkError(char)
+            indexOfCorrupt.append(counter)
             break
+    listOfEndings.append(ending)
+    counter += 1
 
-print(sumError)
+print('Part 1:', sumError)
+
+
+listScores = []
+
+for index in range(len(listOfEndings)): 
+    if index not in indexOfCorrupt:
+        totalScore = 0
+        for bracket in range(len(listOfEndings[index])-1, -1, -1):
+            totalScore *= 5
+            totalScore += checkScore(listOfEndings[index][bracket])
+        listScores.append(totalScore)
+
+print('Part 2:', sorted(listScores)[len(listScores)//2])
