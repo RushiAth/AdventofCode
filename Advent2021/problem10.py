6733f36646dd7705ed9b34ba95472c3748790e99
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-10 16:58:48 -0500

    day10

--- Advent2021/problem10.py
+++ Advent2021/problem10.py
@@ -1,43 +1,21 @@
 handIn = open('input10.txt')
 content = [line.strip() for line in handIn.readlines()]
 
-def checkError(character):
-    if character == ')':
-        return 3
-    elif character == ']':
-        return 57
-    elif character == '}':
-        return 1197
-    else:
-        return 25137
-
-def checkScore(character):
-    if character == ')':
-        return 1
-    elif character == ']':
-        return 2
-    elif character == '}':
-        return 3
-    else:
-        return 4
+errors = {')': 3, ']': 57, '}': 1197, '>': 25137}
+scores = {')': 1, ']': 2, '}': 3, '>': 4}
+brackets = {'(': ')', '[': ']', '{': '}', '<': '>'}
         
-sumError = 0
-counter = 0
+sumError = counter = 0
 indexOfCorrupt = []
 listOfEndings = []
+listScores = []
 
 for line in content:
     ending = []
 
     for char in line:
-        if char == '(':
-            ending.append(')')
-        elif char == '[':
-            ending.append(']')
-        elif char == '{':
-            ending.append('}')
-        elif char == '<':
-            ending.append('>')
+        if char in '([{<':
+            ending.append(brackets[char])
         elif char == ')' and ending[-1] == ')':
             ending.pop()
         elif char == ']' and ending[-1] == ']':
@@ -47,23 +25,19 @@ for line in content:
         elif char == '>' and ending[-1] == '>': 
             ending.pop()
         else:
-            sumError += checkError(char)
+            sumError += errors[char]
             indexOfCorrupt.append(counter)
             break
     listOfEndings.append(ending)
     counter += 1
-
-print('Part 1:', sumError)
-
-
-listScores = []
 
 for index in range(len(listOfEndings)): 
     if index not in indexOfCorrupt:
         totalScore = 0
         for bracket in range(len(listOfEndings[index])-1, -1, -1):
             totalScore *= 5
-            totalScore += checkScore(listOfEndings[index][bracket])
+            totalScore += scores[listOfEndings[index][bracket]]
         listScores.append(totalScore)
 
+print('Part 1:', sumError)
 print('Part 2:', sorted(listScores)[len(listScores)//2])
