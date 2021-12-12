a705cdaea4c3ebe2c2d4b7492b958c4a8078803b
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-11 21:22:36 -0500

    day11

--- Advent2021/problem.11.py
+++ Advent2021/problem.11.py
@@ -2,15 +2,10 @@ handIn = open('input11.txt')
 content = [[11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11]]
 
 for i in range(10): 
-    line = (handIn.readline()).strip()
-    content.append([int(num) for num in line])
-    
-def step(lst):
-    for index in range(1, len(lst)-1):
-        for index2 in range(1, len(lst[index])-1):
-            lst[index][index2] += 1
-    
-    return lst
+    content.append([int(num) for num in (handIn.readline()).strip()])
+    content[i+1].insert(0, 11)
+    content[i+1].append(11)
+content.append([11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11])
 
 def checkFlash(lst):
     for index in range(1, len(lst)-1):
@@ -20,14 +15,16 @@ def checkFlash(lst):
     return False
 
 def flash(lst):
-    flashed = []
     done = []
+    
+    for index in range(1, len(lst)-1):
+        for index2 in range(1, len(lst[index])-1):
+            lst[index][index2] += 1
 
     while checkFlash(lst) == True:
         for index in range(1, len(lst)-1):
             for index2 in range(1, len(lst[index])-1):
                 if lst[index][index2] >= 10 and (index, index2) not in done:
-                    flashed.append(lst[index][index2])
                     lst[index][index2] = 0
                     done.append((index, index2))
 
@@ -48,7 +45,7 @@ def flash(lst):
                     if (index+1, index2+1) not in done:
                         lst[index+1][index2+1] += 1
 
-    return lst, len(flashed)
+    return lst, len(done)
 
 def checkAllFlash(lst):
     for index in range(1, len(lst)-1):
@@ -57,20 +54,15 @@ def checkAllFlash(lst):
                 return False
     return True
 
-for index in range(1, len(content)):
-    content[index].insert(0, 11)
-    content[index].append(11)
-content.append([11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11])
-
 sumFlash = 0
 stepValue = 0
 
 while checkAllFlash(content) == False:
     stepValue += 1
-    content = step(content)
     returning = flash(content)
-    
-    sumFlash += returning[1]
+
+    if stepValue <= 100:
+        sumFlash += returning[1]
     content = returning[0]
 
 print('Part 1:', sumFlash)
