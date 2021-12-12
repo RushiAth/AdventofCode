34f693d958d93fee65c20ae1cab7a11ca41d0a76
Rushi A <69522459+RushiAth@users.noreply.github.com>
2021-12-11 21:12:38 -0500

    day11

--- Advent2021/problem.11.py
+++ Advent2021/problem.11.py
@@ -1,4 +1,4 @@
-handIn = open('testingInput.txt')
+handIn = open('input11.txt')
 content = [[11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11]]
 
 for i in range(10): 
@@ -13,9 +13,9 @@ def step(lst):
     return lst
 
 def checkFlash(lst):
-    for index in range(len(lst)):
-        for index2 in range(len(lst[index])):
-            if lst[index][index2] == 10:
+    for index in range(1, len(lst)-1):
+        for index2 in range(1, len(lst[index])-1):
+            if lst[index][index2] >= 10:
                 return True
     return False
 
@@ -26,21 +26,36 @@ def flash(lst):
     while checkFlash(lst) == True:
         for index in range(1, len(lst)-1):
             for index2 in range(1, len(lst[index])-1):
-                if lst[index][index2] == 10 and (index, index2) not in done:
+                if lst[index][index2] >= 10 and (index, index2) not in done:
                     flashed.append(lst[index][index2])
                     lst[index][index2] = 0
                     done.append((index, index2))
 
-                    lst[index-1][index2-1] += 1
-                    lst[index-1][index2] += 1
-                    lst[index-1][index2+1] += 1
-                    lst[index][index2-1] += 1
-                    lst[index][index2+1] += 1
-                    lst[index+1][index2-1] += 1
-                    lst[index+1][index2] += 1
-                    lst[index+1][index2+1] += 1
+                    if (index-1, index2-1) not in done:
+                        lst[index-1][index2-1] += 1
+                    if (index-1, index2) not in done:
+                        lst[index-1][index2] += 1
+                    if (index-1, index2+1) not in done:
+                        lst[index-1][index2+1] += 1
+                    if (index, index2-1) not in done:
+                        lst[index][index2-1] += 1
+                    if (index, index2+1) not in done:
+                        lst[index][index2+1] += 1
+                    if (index+1, index2-1) not in done:
+                        lst[index+1][index2-1] += 1
+                    if (index+1, index2) not in done:
+                        lst[index+1][index2] += 1
+                    if (index+1, index2+1) not in done:
+                        lst[index+1][index2+1] += 1
 
     return lst, len(flashed)
+
+def checkAllFlash(lst):
+    for index in range(1, len(lst)-1):
+        for index2 in range(1, len(lst[index])-1):
+            if lst[index][index2] != 0:
+                return False
+    return True
 
 for index in range(1, len(content)):
     content[index].insert(0, 11)
@@ -48,14 +63,15 @@ for index in range(1, len(content)):
 content.append([11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11])
 
 sumFlash = 0
+stepValue = 0
 
-for i in range(10):
+while checkAllFlash(content) == False:
+    stepValue += 1
     content = step(content)
-    #for i in content: print(i)
     returning = flash(content)
+    
     sumFlash += returning[1]
     content = returning[0]
-    #for i in content: print(i)
-    #print()
 
-print(sumFlash)
+print('Part 1:', sumFlash)
+print('Part 2:', stepValue)
